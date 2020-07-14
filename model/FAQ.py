005f7b704585f73e505b15cfd5ad1e2af1212282
Xu <xushengquan@souche.com>
2020-07-14 17:40:21 +0800

    fix bug

--- model/FAQ.py
+++ model/FAQ.py
@@ -126,7 +126,7 @@ class FAQ(object):
         logger.info('maxSim=' + format(maxSim.sim, '.0%'))
 
         if maxSim.sim < simCondision:
-            return '抱歉，我没有理解您的意思。请您询问有关汽车的话题。'
+            return '抱歉，我没有理解您的意思。请您询问发布会相关的话题。'
 
         return maxSim.a
 
